e508a4a8ec3bcd7da4dc02a9142cc1f84ae44b63
임종민 <skku.jmlim@gmail.com>
2024-07-05 11:05:14 +0900

    Hand-design Optimizer 추가

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -297,8 +297,8 @@ class MAMLFewShotClassifier(nn.Module):
                     # 2) Calculate moving average of weight norm
                     for key, weight in names_weights_copy.items():
                         weight_norm = torch.norm(weight, p=2)
-                        ema_calculator_wn.update(key, weight_norm)
                         ema_value_wn = ema_calculator_wn.get_EMA(key)
+                        ema_calculator_wn.update(key, weight_norm)
 
                         per_step_task_embedding.append(ema_value_wn)
 
@@ -310,8 +310,8 @@ class MAMLFewShotClassifier(nn.Module):
                     # 4) Calculate moving average of gradient norm
                     for key, grad in names_grads_copy.items():
                         gradient_norm = torch.norm(grad, p=2)
-                        ema_calculator_gn.update(key, gradient_norm)
                         ema_value_gn = ema_calculator_gn.get_EMA(key)
+                        ema_calculator_gn.update(key, gradient_norm)
 
                         per_step_task_embedding.append(ema_value_gn)
 
