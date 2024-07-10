92b21ee06010a195cf663ff1647e3da8a3273bd2
임종민 <skku.jmlim@gmail.com>
2024-07-10 20:25:07 +0900

    diffGrad 추가

--- inner_loop_optimizers_Adam.py
+++ inner_loop_optimizers_Adam.py
@@ -117,8 +117,8 @@ class GradientDescentLearningRule(nn.Module):
 
                 if self.args.momentum == "Adam":
 
-                    # weight_decay = 0.05
-                    # applied_gradient += weight_decay * names_weights_dict[key]
+                    weight_decay = 0.001
+                    applied_gradient += weight_decay * names_weights_dict[key]
 
                     # Update biased first moment estimate
                     self.m[key] = self.beta1 * self.m[key] + (1 - self.beta1) * applied_gradient
@@ -132,11 +132,13 @@ class GradientDescentLearningRule(nn.Module):
                     # Compute bias-corrected second moment estimate
                     v_hat = self.v[key] / (1 - self.beta2 ** (num_step+1))
 
-                    lr_t = self.learning_rate / (1 - self.beta1 ** (num_step + 1))
+                    # lr_t = self.learning_rate / (1 - self.beta1 ** (num_step + 1))
+                    lr_t = self.learning_rate * torch.sqrt(torch.tensor(1 - self.beta2 ** (num_step + 1))) / (
+                            1 - self.beta1 ** (num_step + 1))
 
                     # Adam Update
                     updated_names_grads_wrt_params_dict[key] = applied_gradient
-                    updated_names_weights_dict[key] = names_weights_dict[key] - lr_t / (torch.sqrt(v_hat + self.epsilon)) * m_hat
+                    updated_names_weights_dict[key] = names_weights_dict[key] - lr_t * m_hat / (torch.sqrt(v_hat + self.epsilon))
 
                 else:
                     # SGD Update
