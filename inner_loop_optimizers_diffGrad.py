d7486025eb21b78baa43283ab31b79a46eea3877
임종민 <skku.jmlim@gmail.com>
2024-07-10 19:42:06 +0900

    diffGrad 추가

--- inner_loop_optimizers_diffGrad.py
+++ inner_loop_optimizers_diffGrad.py
@@ -146,9 +146,7 @@ class GradientDescentLearningRule(nn.Module):
                     v_hat = self.v[key] / (1 - self.beta2 ** (num_step+1))
 
                     # Apply diffgrad term
-                    m_hat *= diff
-
-                    lr_t = self.learning_rate * torch.sqrt(torch.tensor(1 - self.beta2 ** (num_step + 1))) / (1 - self.beta1 ** (num_step + 1))
+                    lr_t = self.learning_rate * dfc * torch.sqrt(torch.tensor(1 - self.beta2 ** (num_step + 1))) / (1 - self.beta1 ** (num_step + 1))
 
                     updated_names_weights_dict[key] = names_weights_dict[key] - lr_t * m_hat / (torch.sqrt(v_hat + self.epsilon))
                     updated_names_grads_wrt_params_dict[key] = applied_gradient
