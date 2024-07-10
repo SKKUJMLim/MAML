59de966d998015740cfc31c57bde8f3266a2f5fb
임종민 <skku.jmlim@gmail.com>
2024-07-10 21:27:48 +0900

    diffGrad 추가

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -292,10 +292,11 @@ class MAMLFewShotClassifier(nn.Module):
                     # 1) Calculate weight norm && moving average of weight norm
                     for key, weight in names_weights_copy.items():
                         weight_norm = torch.norm(weight, p=2)
-                        ema_value_wn = ema_calculator_wn.get_EMA(key)
 
                         if num_step == 0:
                             ema_calculator_wn.update(key, weight_norm)
+
+                        ema_value_wn = ema_calculator_wn.get_EMA(key)
 
                         per_step_task_embedding.append(weight_norm)
                         per_step_task_embedding.append(ema_value_wn)
@@ -303,10 +304,11 @@ class MAMLFewShotClassifier(nn.Module):
                     # 2) Calculate gradient norm && moving average of gradient norm
                     for key, grad in names_grads_copy.items():
                         gradient_norm = torch.norm(grad, p=2)
-                        ema_value_gn = ema_calculator_gn.get_EMA(key)
 
                         if num_step == 0:
                             ema_calculator_gn.update(key, gradient_norm)
+
+                        ema_value_gn = ema_calculator_gn.get_EMA(key)
 
                         per_step_task_embedding.append(gradient_norm)
                         per_step_task_embedding.append(ema_value_gn)
