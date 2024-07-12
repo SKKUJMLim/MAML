cee0350412f61d8165d5a9adbe386bada9fae502
임종민 <skku.jmlim@gmail.com>
2024-07-12 11:39:36 +0900

    MAML filter 128 실험

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -292,6 +292,10 @@ class MAMLFewShotClassifier(nn.Module):
                     # 1) Calculate weight norm && moving average of weight norm
                     for key, weight in names_weights_copy.items():
                         weight_norm = torch.norm(weight, p=2)
+
+                        if num_step==0:
+                            ema_calculator_wn.update(key, weight_norm)
+
                         ema_value_wn = ema_calculator_wn.get_EMA(key)
 
                         per_step_task_embedding.append(weight_norm)
@@ -300,6 +304,10 @@ class MAMLFewShotClassifier(nn.Module):
                     # 2) Calculate gradient norm && moving average of gradient norm
                     for key, grad in names_grads_copy.items():
                         gradient_norm = torch.norm(grad, p=2)
+
+                        if num_step==0:
+                            ema_calculator_gn.update(key, gradient_norm)
+
                         ema_value_gn = ema_calculator_gn.get_EMA(key)
 
                         per_step_task_embedding.append(gradient_norm)
