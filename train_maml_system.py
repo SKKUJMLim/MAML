9169c23cc745b3f7a454079220fe50e34d1dcc4b
임종민 <skku.jmlim@gmail.com>
2024-08-05 20:01:43 +0900

    실험 세팅

--- train_maml_system.py
+++ train_maml_system.py
@@ -45,7 +45,11 @@ from multiprocessing import freeze_support
 # 2) tiered imagenet
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_tiered_imagenet_5way_1shot.json --gpu_to_use 0
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_tiered_imagenet_5way_5shot.json --gpu_to_use 0
+
+## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_tired_imagenet_5way_1shot_filter48.json --gpu_to_use 0
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_tired_imagenet_5way_5shot_filter48.json --gpu_to_use 0
+
+## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_tired_imagenet_5way_1shot_filter64.json --gpu_to_use 0
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_tired_imagenet_5way_5shot_filter64.json --gpu_to_use 0
 
 
